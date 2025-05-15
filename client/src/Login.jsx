1d60a294ee02453f58dedb79898589bb8bf24d20
Rohan <messilionelmesi289@gmail.com>
2025-05-16 01:23:25 +0530

    Implement Registration And Login

---
+++ client/src/Login.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import axios from 'axios';
+function Login() {
+    const [name , setName] = useState('');
+    const [password , setPassword] = useState('');
+  
+    const submitForm = async (e) => {
+        e.preventDefault();
+        const data = {
+            Username: name,
+            Password: password
+        }
+        try{
+            const response = await axios.post('http://localhost:3000/api/login', data);
+            localStorage.setItem('token', response.data.token);
+            console.log(response.data);
+           if (response.data.token) {
+    alert('Login successful');
+} else {
+    alert('Login failed');
+}
+
+        }
+       catch (error) {
+    console.error('Error:', error);
+    alert(error.response?.data?.message || 'An error occurred during Login');
+}
+
+        
+    }
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={submitForm}>
+            <input placeholder="Name" type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            <br />
+           
+            <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <br />
+            <button type="submit">Login</button>
+
+        </form>
+        </div>
+        
+    );
+}
+export default Login;
---
+++ client/src/Register.jsx
@@ -0,0 +1,52 @@
+import {useState} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+function Register() {
+    const [name , setName] = useState('');
+    const [email , setEmail] = useState('');
+    const [password , setPassword] = useState('');
+    const submitForm = async (e) => {
+        e.preventDefault();
+        const data = {
+            Username: name,
+            Email: email,
+            Password: password
+        }
+        try{
+            const response = await axios.post('http://localhost:3000/api/register', data);
+            console.log(response.data);
+            alert(response.data.message || 'Registration successful');
+
+        }
+        catch (error) {
+    console.error('Error:', error);
+    alert(error.response?.data?.message || 'An error occurred during registration');
+}
+
+        
+    }
+    return (
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={submitForm}>
+            <input placeholder="Name" type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+            <br />
+            <input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+            <br />
+            <input placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+            <br />
+            <button type="submit">Register</button>
+
+        </form>
+        <p className="mt-4">
+  Already have an account?{' '}
+  <Link to="/login" className="text-blue-500 underline">
+    Login here
+  </Link>
+</p>
+
+        </div>
+        
+    );
+}
+export default Register;
